623f2b5c7bff6804be0c2c1f767d7df35f50b952
Bryce Tsuyuki <btsuyuki1@gmail.com>
2019-02-24 23:45:53 -0800

    update stuff

--- iPhoneBatteryClient WatchKit Extension/InterfaceController.swift
+++ iPhoneBatteryClient WatchKit Extension/InterfaceController.swift
@@ -7,10 +7,16 @@
 //
 
 import WatchKit
+import UIKit
 import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        // stuff happens, ok
+    }
+    
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -21,11 +27,70 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        if WCSession.isSupported() {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    func updateAWBatteryPercent() {
+        
+        // convert to string
+        let batteryStr = convertBatteryPercentToString()
+        
+        // set label
+        AWBatteryLabel.setText(batteryStr)
+    }
+    
+    func convertBatteryPercentToString() -> String
+    {
+        // enable battery monitoring
+        WKInterfaceDevice.current().isBatteryMonitoringEnabled = true
+        
+        // setup the number formatter
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        
+        let battStr : String! = nf.string(from: NSNumber(value: awBatteryLevel*100))
+        return battStr
+    }
+    
+    @IBAction func UpdatePercentsButton() {
+        updateAWBatteryPercent()
+    }
+    
+    @IBAction func SendToiPhoneButton() {
+        let battPercString = convertBatteryPercentToString()
+        let dict: Dictionary = ["message": battPercString]
+        let session : WCSession = WCSession.default;
+        session.sendMessage(dict, replyHandler: nil, errorHandler: {(error ) -> Void in
+            // catch any errors here
+        })
 
+    }
+    
+    
+    
+    var awBatteryLevel: Float {
+        return WKInterfaceDevice.current().batteryLevel
+    }
+    
+    @IBOutlet weak var AWBatteryLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var iPhoneBatteryLabel: WKInterfaceLabel!
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        
+        let battStr = message["Status"] as! String
+        
+        iPhoneBatteryLabel.setText(battStr)
+    }
 }
--- iPhoneBatteryClient/ViewController.swift
+++ iPhoneBatteryClient/ViewController.swift
@@ -7,14 +7,99 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WCSessionDelegate{
+    
+    var lastMessage: CFAbsoluteTime = 0
+
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        // nothing happens i guess
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        // nothing yet
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        // nothing yet
+    }
+    
+    //MARK: Properties
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        updateiPhoneBatteryPercent()
+        
+        // communication setup
+        if (WCSession.isSupported()) {
+            let session = WCSession.default
+            session.delegate = self;
+            session.activate()
+            
+        }
+    }
+    
+    func updateiPhoneBatteryPercent() {
+        // convert to string
+        let batteryStr = convertBatteryPercentToString()
+        
+        // set label
+        iPhonePercentLabel.text = batteryStr
+        
+    }
+    
+    func convertBatteryPercentToString() -> String
+    {
+        // enable battery monitoring
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        
+        // setup the number formatter
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        
+        let battStr : String! = nf.string(from: NSNumber(value: iphoneBatteryLevel*100))
+        return battStr
+    }
+    
+    @IBOutlet weak var iPhonePercentLabel: UILabel!
+    
+    @IBAction func UpdateiPhoneButton(_ sender: Any) {
+        updateiPhoneBatteryPercent()
+    }
+    var iphoneBatteryLevel: Float {
+        return UIDevice.current.batteryLevel
     }
 
+    
+    @IBAction func SendToWatchButton(_ sender: Any) {
+        sendWatchMessage()
+    }
+    
+    
+    func sendWatchMessage() {
+        let currentTime = CFAbsoluteTimeGetCurrent()
+        
+        // if less than half a second has passed, bail out
+        if lastMessage + 0.5 > currentTime {
+            return
+        }
+        
+        // send a message to the watch if it's reachable
+        if (WCSession.default.isReachable) {
+            let battStr = convertBatteryPercentToString()
+            let message = ["Status": battStr]
+            WCSession.default.sendMessage(message, replyHandler: nil)
+        }
+        
+        // update our rate limiting property
+        lastMessage = CFAbsoluteTimeGetCurrent()
+    }
+    
 
 }
 
